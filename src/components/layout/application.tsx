0bfc3b7fcb3d303291938a61f0cbff1f9c88d42f
ihavecoke <10757551+ihavecoke@users.noreply.github.com>
2024-06-20 10:32:49 +0800

    update meta info (#17)
    update meta info
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: LongPort wiki bot <longport-wiki@users.noreply.github.com>

--- src/components/layout/application.tsx
+++ src/components/layout/application.tsx
@@ -1,15 +1,17 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import React, { FC } from "react";
+import { translate } from "@docusaurus/Translate";
 
 const ApplicationLayout: FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`${siteConfig.title}`} description="LongPort Wiki">
+    <Layout
+      title={translate({ id: "meta_title" })}
+      description={translate({ id: "meta_desc" })}
+    >
       <div className="max-w-screen-xl m-auto px-4 md:px-0 wiki-article-content">
         {children}
       </div>
     </Layout>
-  )
+  );
 };
 export default ApplicationLayout;
