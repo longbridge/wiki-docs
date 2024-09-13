436e1c54c22c551260ac91ba877fe4be03680119
Clifton Hills <ylinwind@163.com>
2024-09-13 13:00:26 +0800

    fix: 新增 wiki alias (#42)
    Co-authored-by: linxin <xin.lin@longbridge-inc.com>

--- src/components/ai-content/index.tsx
+++ src/components/ai-content/index.tsx
@@ -9,9 +9,9 @@ interface AIContentProps {
 }
 const LocalesMap = {
   aiGen: {
-    "zh-CN": "以下内容是 AI 的进一步解读",
-    "zh-HK": "以下內容是 AI 的進一步解讀",
-    en: "The following content is a further interpretation by AI.",
+    "zh-CN": "以上内容是 AI 的进一步解读",
+    "zh-HK": "以上內容是 AI 的進一步解讀",
+    en: "The above content is a further interpretation by AI.",
   },
   disclaimer: {
     "zh-CN": "免责声明",
@@ -43,22 +43,26 @@ export const AIContent: React.FC<AIContentProps> = ({ content, id }) => {
   if (!AIContent) return <></>;
   return (
     <div className="ai-content pt-8 rounded-lg">
-      <div className="flex items-center space-x-4 pb-3 mb-3 border-solid border-b border-t-0 border-r-0 border-l-0 border-[var(--ifm-color-gray-300)]">
+      <div
+        dangerouslySetInnerHTML={{ __html: AIContent }}
+        className="border-solid border-t border-b-0 border-r-0 border-l-0 border-[var(--ifm-color-gray-300)] pt-5"
+      ></div>
+
+      <div className="flex items-center space-x-4 pt-3 mb-3 border-solid border-t border-b-0 border-r-0 border-l-0 border-[var(--ifm-color-gray-300)]">
         <img
           src="https://assets.lbctrl.com/uploads/831466be-e4f8-46a7-aa0b-efcda7a424ff/portai.png"
           alt="port-ai"
           className="w-7 h-7 rounded-full"
         />
         <span className="text-sm">{LocalesMap.aiGen[locale]}</span>
+        <a
+          href="https://support.longbridgewhale.com/topics/misc/portaidisclaimer"
+          className="text-sm text-[#37a0ff]"
+          target="_blank"
+        >
+          {LocalesMap.disclaimer[locale]}
+        </a>
       </div>
-      <div dangerouslySetInnerHTML={{ __html: AIContent }}></div>
-      <a
-        href="https://support.longbridgewhale.com/topics/misc/portaidisclaimer"
-        className="text-sm text-[#37a0ff]"
-        target="_blank"
-      >
-        {LocalesMap.disclaimer[locale]}
-      </a>
     </div>
   );
 };
